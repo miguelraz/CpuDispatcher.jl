a89d310783cccc7647bc7428eac0a27fcea356cd
miguel raz <miguelraz@ciencias.unam.mx>
2019-07-27 17:29:07 -0500

    Files generated by PkgTemplates

---
+++ src/CpuDispatcher.jl
@@ -0,0 +1,5 @@
+module CpuDispatcher
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CpuDispatcher
+using Test
+
+@testset "CpuDispatcher.jl" begin
+    # Write your own tests here.
+end
